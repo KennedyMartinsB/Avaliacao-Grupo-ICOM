16827e751989e67bd8807327e42d27c64608160c
KennedyMartinsB <kenedy.martins185@gmail.com>
2025-08-27 23:07:13 -0300

    Feature: função de deletar jogo

--- avaliacao-icom/src/app/components/item-list/item-list.component.ts
+++ avaliacao-icom/src/app/components/item-list/item-list.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { JogoService } from 'src/app/service/jogo.service';
 
 @Component({
   selector: 'app-item-list',
@@ -13,5 +16,24 @@ export class ItemListComponent {
   @Input() produtora: string | undefined;
   @Input() ano: number | undefined;
   @Input() idadeMinima: number | undefined;
+
+  private deleteSubscribe: Subscription | undefined;
+
+  constructor(private jogoService: JogoService, private httpClient: HttpClient) {}
+
+  deleteGame(id: any): void{
+    this.deleteSubscribe = this.jogoService.deleteGame(id).subscribe({
+      next: () => {
+        console.log("Jogo excluido com sucesso!!!");
+        window.location.reload()
+      },
+      error: (error: Error) => {
+        console.log("Erro ao excluir empresa: ", error)
+      },
+      complete: () => {
+        console.log("Operação de exclusão foi feita com sucesso!!!");
+      }
+    })
+  }
 
 }
