9ac6be51c25411e7b82115af95e67795c894f48d
KennedyMartinsB <kenedy.martins185@gmail.com>
2025-08-29 03:15:55 -0300

    Feature: criação e listagem de jogo

--- avaliacao-icom/src/app/components/cadastro-form/cadastro-form.component.ts
+++ avaliacao-icom/src/app/components/cadastro-form/cadastro-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { novoJogo } from 'src/app/interface/jogo';
+import { Router } from '@angular/router';
+import { Jogo, novoJogo } from 'src/app/interface/jogo';
 import { JogoService } from 'src/app/service/jogo.service';
 
 @Component({
@@ -11,7 +12,7 @@ import { JogoService } from 'src/app/service/jogo.service';
 export class CadastroFormComponent implements OnInit{
   cadastroForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private jogoService: JogoService) {}
+  constructor(private formBuilder: FormBuilder, private jogoService: JogoService, private router: Router) {}
 
   ngOnInit() {
     this.cadastroForm = this.formBuilder.group({
@@ -27,6 +28,21 @@ export class CadastroFormComponent implements OnInit{
     if(this.cadastroForm.valid){
       const novoJogo : novoJogo = this.cadastroForm.value;
       console.log("Dados do cadastro: ", novoJogo)
+
+      this.jogoService.postGame(novoJogo).subscribe({
+        next: (Response) => {
+          console.log("Cadastro realizado com sucesso!!!")
+          console.log("Objeto enviado: ", novoJogo)
+          this.cadastroForm.reset()
+        },
+         error: (error: Error) => {
+          console.error('Erro no cadastro do jogo:', error);
+        },
+        complete: () => {
+          console.log('Operação de cadastro de empresa concluída.');
+          this.router.navigate(['']);
+        }
+      })
     }
   }
 }
--- avaliacao-icom/src/app/service/jogo.service.ts
+++ avaliacao-icom/src/app/service/jogo.service.ts
@@ -20,27 +20,50 @@ export class JogoService {
     const del_Url = `${this.API_URL}/${id}`;
     return this.httpClient.delete<void>(del_Url);
   }
-  postJogo(jogo: novoJogo): Observable<{ sucesso: boolean } | string> {
-    return this.httpClient.post<Jogo>(this.API_URL, jogo).pipe(
+
+  postGame(novoJogo: novoJogo): Observable<{ success: boolean } | string> {
+    return this.httpClient.post<Jogo>(this.API_URL, novoJogo).pipe(
       // Mapeia a resposta de sucesso
       map(() => {
         // Retorna um objeto JSON com sucesso: true
-        return { sucesso: true };
+        return { success: true };
       }),
-      // Captura o erro
       catchError((error: HttpErrorResponse) => {
-        let mensagemDeErro = 'Erro desconhecido ao realizar a requisição.';
-
+        let messageError = 'Erro desconhecido ao realizar a requisição.';
         if (error.error instanceof ErrorEvent) {
-          // Erro no lado do cliente ou na rede
-          mensagemDeErro = `Erro: ${error.error.message}`;
+          messageError = `Erro: ${error.error.message}`;
         } else {
-          // Erro no lado do servidor
-          mensagemDeErro = `Erro no servidor: ${error.status} - ${error.message}`;
+          messageError = `Erro no servidor: ${error.status} - ${error.message}`;
         }
-        // Retorna um Observable com uma string de erro
-        return throwError(() => mensagemDeErro);
+        return throwError(() => messageError);
       })
     );
   }
+
+  putGame(jogoAtualizado: Jogo): Observable<{sucess: Boolean} | string> {
+    return this.httpClient.put<Jogo>(this.API_URL, {
+      id: jogoAtualizado.id, nome: jogoAtualizado.nome, ano: jogoAtualizado.ano,
+      produtora: jogoAtualizado.produtora, idadeMinima: jogoAtualizado.idadeMinima,
+      descricao: jogoAtualizado.descricao
+    }).pipe(
+      map(() => {
+        return { sucess: true }
+      }),
+      catchError((error: HttpErrorResponse)=>{
+        let messageError = 'Erro ao realizar a requisição de update.';
+        if (error.error instanceof ErrorEvent) {
+          messageError = `Erro: ${error.error.message}`;
+        } else {
+          messageError = `Erro no servidor: ${error.status} - ${error.message}`;
+        }
+        return throwError(() => messageError);
+      })
+    )
+  }
+
+  getGameById(id: any): Observable<Jogo>{
+    const url = `${this.API_URL}/${id}`
+    return this.httpClient.get<Jogo>(url);
+  }
+
 }
